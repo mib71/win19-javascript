e4fa1ac48d400742ad0e67d6dfe3ad1f85c8bbd0
MiB <mikael.barnholdt@hotmail.com>
2020-01-07 16:44:03 +0100

    Add exercises Medium text fix

--- exercisesMedium/alternateList.js
+++ exercisesMedium/alternateList.js
@@ -1,4 +1,5 @@
-/**Write a function alternate that combines two lists by alternatingly taking elements. */
+//Write a function alternate that combines two lists by
+// alternatingly taking elements.
 // Example
 // alternate(['a', 'b', 'c'], [1, 2, 3])
 // -> ['a', 1, 'b', 2, 'c', 3]

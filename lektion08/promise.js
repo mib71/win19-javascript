76c796e58536cc86fc4d1aa18f3f08f75f687c67
MiB <mikael.barnholdt@hotmail.com>
2020-01-08 11:32:13 +0100

    prmoise fix

--- lektion08/promise.js
+++ lektion08/promise.js
@@ -1,3 +1,14 @@
+// En Promise är ett objekt där det finns
+// två vanliga metoder vi använder hela tiden.
+// Nämligen then & catch. Dessa metoderna
+// berättar för promisen vilken kod vi vill
+// ska göras när den asynkrona koden är
+//färdig. then metoden berättar vilken funktion
+// som ska köras om resolve anropas av
+// promisen. catch metoden berättar vilken
+// funktion som ska köras om reject anropas
+// av promisen.
+
 const giveMePromise = new Promise(function(resolve) {
   setTimeout(function() {
     resolve("Hello from promise");

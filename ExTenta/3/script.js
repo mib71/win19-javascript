7e0944714ccbcaff26d9991f00ed760cc85d2133
MiB <mikael.barnholdt@hotmail.com>
2020-01-08 18:50:09 +0100

    Add try and error

--- ExTenta/3/script.js
+++ ExTenta/3/script.js
@@ -7,6 +7,53 @@
 
 /* Implement your solution here */
 
+function renderTable(arr) {
+  let tableEl = document.createElement("table");
+  let theadEl = document.createElement("thead");
+  let tbodyEl = document.createElement("tbody");
+  let trEl = document.createElement("tr");
+
+  tableEl.style.border = 1;
+
+  let thNameEl = document.createElement("th");
+  let thOccEl = document.createElement("th");
+  let thAgeEl = document.createElement("th");
+
+  thNameEl.textContent = "Name";
+  thOccEl.textContent = "Occupation";
+  thAgeEl.textContent = "Age";
+
+  document.querySelector("main").appendChild(tableEl);
+  tableEl.appendChild(theadEl);
+  tableEl.appendChild(tbodyEl);
+
+  theadEl.appendChild(trEl);
+
+  trEl.appendChild(thNameEl);
+  trEl.appendChild(thOccEl);
+  trEl.appendChild(thAgeEl);
+
+  for (i = 0; i < arr.length; i++) {
+    // Skapa element
+    let trEl = document.createElement("tr");
+
+    let tdNameEl = document.createElement("td");
+    let tdOccEl = document.createElement("td");
+    let tdAgeEl = document.createElement("td");
+
+    // Ge elementen ett innehåll
+    tdNameEl.textContent = arr[i].name;
+    tdOccEl.textContent = arr[i].occupation;
+    tdAgeEl.textContent = arr[i].age;
+
+    // Sätt ut elementen i DOMen
+    tbodyEl.appendChild(trEl);
+    trEl.appendChild(tdNameEl);
+    trEl.appendChild(tdOccEl);
+    trEl.appendChild(tdAgeEl);
+  }
+}
+
 /* Do not touch anything below this line */
 let data = [
   { name: "Rachel Green", occupation: "Waitress", age: 24 },

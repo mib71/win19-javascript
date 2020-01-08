9f6465023a4a82e1cd97848cdf1ae6f4ff11ece3
MiB <mikael.barnholdt@hotmail.com>
2020-01-08 13:34:57 +0100

    bug fix

--- lektion08/rekursivFakultet.js
+++ lektion08/rekursivFakultet.js
@@ -1,6 +1,13 @@
+// En fakultet i Matematiken ser ut som
+// följande: 5! = 5 * 4 * 3 * 2 * 1
+// Denna logiken går enkelt att skapa i
+//programmering med en rekursiv funktion.
+
 function factorial(n) {
   if (n === 1) {
-    return n;
+    return 1;
   }
   return n * factorial(n - 1);
 }
+
+console.log(factorial(5));
